e168b563c482699d02eee10f8a87a8fbdd190383
Archer <Archer>
2016-11-10 11:31:21 +0630

    Committing

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+var router = express.Router();
+var path = __dirname+'/views/';
+
+
+
+router.get("/",function(req,res){
+  res.sendFile(path + "index.html");
+});
+
+
+
+
+app.use("/",router);
+app.use('/views', express.static('views'))
+
+
+app.listen(8080,function(){
+  console.log("art.com @ Port 8080");
+});
