2165f852a10a27c720efa480b3a36d12691577ad
Archer <Archer>
2016-12-01 13:58:38 +0630

    Facebook Integration

--- server.js
+++ server.js
@@ -9,10 +9,20 @@ router.get("/",function(req,res){
   res.sendFile(path + "index.html");
 });
 
+router.get("/price",function(req,res){
+  res.json("this gonna be oops route");
+});
+
+router.get("/api/price",function(req,res){
+  res.json("viola");
+});
+
 
 
 
 app.use("/",router);
+app.use("/price",router);
+app.use("/api",router);
 app.use('/views', express.static('views'))
 
 app.listen(process.env.PORT || 5000);
